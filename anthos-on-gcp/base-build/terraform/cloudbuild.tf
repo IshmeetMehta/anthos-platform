fb57237d5deb42507e9026d1dd09cc83f88993a7
Ishmeet Mehta <ishmeetm@google.com>
2022-01-25 17:30:41 -0600

    Add cloud build trigger

---
+++ anthos-on-gcp/base-build/terraform/cloudbuild.tf
@@ -0,0 +1,33 @@
+# data "google_project" "project" {
+#      project = var.project_id
+# }
+
+resource "google_cloudbuild_trigger" "service-account-trigger" {
+  trigger_template {
+    branch_name = "master"
+    repo_name   = "https://github.com/IshmeetMehta/container-app"
+  }
+
+  service_account = google_service_account.cloudbuild_service_account.id
+  filename        = "cloudbuild.yaml"
+  depends_on = [
+    google_project_iam_member.act_as,
+    google_project_iam_member.logs_writer
+  ]
+}
+
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id = "my-service-account"
+}
+
+resource "google_project_iam_member" "act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
